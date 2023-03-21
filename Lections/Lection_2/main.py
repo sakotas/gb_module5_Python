8aaaedb792a6fcb98b39bc7765a88cf80f60edf2
Aleksandr Kotishevskii <kotishevskiyaa@gmail.com>
2023-03-21 16:23:32 +0300

    Seminar_2 solved

---
+++ Lections/Lection_2/main.py
@@ -0,0 +1,28 @@
+import random as rnd
+
+list_1 = []
+# list_1 = list()
+# list_1 = [1, 2, 5, 6, 8]
+
+# print(*list_1)
+
+# for i in list_1:
+#     print(i)
+
+# print(len(list_1))
+
+# print(list_1[0])
+
+# list_1.append(8)
+# print(list_1)
+
+# print(list_1)
+#
+# for i in range(5):
+#     list_1.append(rnd.randint(1, 10))
+#
+# print(list_1)
+
+t = ()
+
+print(type(t))
